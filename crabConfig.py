fe19965fb6648614d3bac9cb63d9c95e544b104a
Henrique Legoinha <88343788+Legoinha@users.noreply.github.com>
2024-03-06 17:15:45 +0000

    Add files via upload

--- crabConfig.py
+++ crabConfig.py
@@ -1,26 +1,24 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = '2017ppMCBP230706'
+config.General.requestName = '2017ppMCBP_hl_TEST'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
-#config.JobType.psetName = 'runForestAOD_pp_MC_94X_onlyBfinder.py'
+config.JobType.psetName = 'runForestAOD_pp_MC_94X_onlyBfinder.py'
 
-config.JobType.psetName = 'runForestAOD_pp_MC_94X_BP.py'
+#config.JobType.psetName = 'runForestAOD_pp_MC_94X_BP.py'
 config.JobType.numCores = 1
 #config.JobType.maxMemoryMB = 20000
 config.JobType.maxMemoryMB = 2000
 #config.JobType.allowUndistributedCMSSW = True
 config.Site.ignoreGlobalBlacklist = True
+
 #config.Data.inputDataset = '/HIMinimumBias0/HIRun2018A-04Apr2019-v1/AOD'
-
 #config.Data.inputDataset = 'runForestAOD_pp_MC_94X_onlyBfinder.py'
-
 #config.Data.inputDataset = '/BuToJpsiKp_pThat-5_TuneCP5_5p02TeV_Pythia8/RunIIpp5Spring18DR-94X_mc2017_realistic_forppRef5TeV_v1-v1/AODSIM'
-
 #config.Data.inputDataset ='/BJPsiMM_TuneCUETP8M1_5p02TeV_pythia8/RunIIpp5Spring18MiniAOD-94X_mc2017_realistic_forppRef5TeV-v2/MINIAODSIM'
 
 config.Data.inputDataset = '/BuToJpsiKaon_pThat-0_TuneCP5_PtGT2_5p02TeV_pythia8_evtgen/RunIIpp5Spring18DR-94X_mc2017_realistic_forppRef5TeV_v2-v1/AODSIM'
@@ -31,8 +29,9 @@ config.Data.inputDataset = '/BuToJpsiKaon_pThat-0_TuneCP5_PtGT2_5p02TeV_pythia8_
 
 #config.Data.lumiMask = "v1_JSON.txt"
 #config.Data.splitting = 'FileBased'
-config.Data.inputDBS = "global"                                                                                                                                                   
-config.Data.splitting = "LumiBased"                                                                                                                                              
+
+config.Data.inputDBS = "global"                                                     
+config.Data.splitting = "LumiBased"                                                 
 config.Data.unitsPerJob = 20
 
 #config.Data.lumiMask = 'Cert_306546-306826_5TeV_EOY2017ReReco_Collisions17_JSON_MuonPhys.txt'
@@ -41,11 +40,11 @@ config.JobType.allowUndistributedCMSSW = True
 
 #NJOBS = 500  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = -1
-#config.Data.outLFNDirBase = '/store/user/zzshi/' 
-# config.Data.outLFNDirBase = '/store/group/phys_heavyions/zshi/2017ppMCNew/NPJPsi/BsONLY'
-config.Data.outLFNDirBase = '/store/user/tsheng/2017ppBPMC'
+ 
+config.Data.outLFNDirBase = '/store/user/hmarques'
+
 config.Data.publication = False
-config.Data.outputDatasetTag = '2017ppMCNPJPsiv3'
+config.Data.outputDatasetTag = '2017ppMCNPJPsi_hl_TEST'
 
 
 config.Site.storageSite = 'T2_US_MIT'
